3c315eab6d041870d222e19c5cd399220d44b51e
ghallihan <30986870+ghallihan@users.noreply.github.com>
2020-07-31 17:14:38 -0400

    refactorh

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-
 Amplify.configure(config);
 
 ReactDOM.render(
